9045a3e3291be83d7b1489c9040fe0084694aeae
Fábio <fabiopires1011@gmail.com>
2022-01-12 19:27:34 +0000

    Evidences

---
+++ ExfiltracaoDeDados/src/main/java/org/example/model/Evidence.java
@@ -0,0 +1,34 @@
+package org.example.model;
+
+
+public class Evidence extends Fact {
+    public static final String DEFENSES = "Are there defenses";
+    public static final String HTPP_ALLOWED = "Is HTPP connections allowed to outside";
+    public static final String DOMAIN_CHECKED = "Is the domain checked";
+    public static final String ENCRYPTION = "Is encryption needed";
+    public static final String ACCEPTED_DOMAIN = "Do we own an accepted domain";
+    public static final String DNS_ALLOWED = "Are DNS queries allowed";
+    public static final String HIGH_AMOUNT_DATA = "Is the amount of data high";
+    public static final String SSH_ALLOWED = "Are SSH connections allowed";
+    public static final String EMAIL_ALLOWED = "Is email communication permitted";
+
+    private String evidence;
+    private String value;
+
+    public Evidence(String ev, String v) {
+        evidence = ev;
+        value = v;
+    }
+
+    public String getEvidence() {
+        return evidence;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return (evidence + " = " + value);
+    }
+}
